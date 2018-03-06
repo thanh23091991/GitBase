93bc1eaef02c54f93049af93b3ad585c2d33600e
dothanh <dothanh.ptit@gmail.com>
2018-03-06 20:32:17 +0700

    thanh

--- CustomerManager/src/CustomerManager/Controller.java
+++ CustomerManager/src/CustomerManager/Controller.java
@@ -13,6 +13,7 @@ public class Controller {
 		//ducnv5   
 		//iuefy
 		//ducnv5 222222 06032018
+		//atfhgf thanh
 		System.out.println("--- Table of Content ---");
 		System.out.println("1. Nháº­p thÃ´ng tin khÃ¡ch hÃ ng?");
 		System.out.println("2. Hiá»ƒn thá»‹ danh sÃ¡ch khÃ¡ch hÃ ng?");
