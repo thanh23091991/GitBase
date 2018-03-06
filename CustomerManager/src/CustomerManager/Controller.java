4e14f15149d98914e9a3eec1e0714980180599eb
DucNV <nvdduc@gmail.com>
2018-03-06 20:22:54 +0700

    0603

--- CustomerManager/src/CustomerManager/Controller.java
+++ CustomerManager/src/CustomerManager/Controller.java
@@ -12,6 +12,7 @@ public class Controller {
 	public void TableOfConten() {
 		//ducnv5   
 		//iuefy
+		//ducnv5 222222 06032018
 		System.out.println("--- Table of Content ---");
 		System.out.println("1. Nháº­p thÃ´ng tin khÃ¡ch hÃ ng?");
 		System.out.println("2. Hiá»ƒn thá»‹ danh sÃ¡ch khÃ¡ch hÃ ng?");
