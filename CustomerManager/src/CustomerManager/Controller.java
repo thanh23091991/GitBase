f7e8a829ca82a86393d38dd7988108842df14d5b
T2D <T2D@thanhdd>
2018-03-06 20:16:15 +0700

    âsá

--- CustomerManager/src/CustomerManager/Controller.java
+++ CustomerManager/src/CustomerManager/Controller.java
@@ -21,7 +21,7 @@ public class Controller {
 		System.out.println("==> Input?");
 	}
 	public void Edit() {
-		
+		//adasdasdasdasdasdasdas
 		System.out.println("MÃ£ khÃ¡ch hÃ ng?");
 		model1.RollNo = Integer.parseInt(sc.nextLine());
 		System.out.println("TÃªn khÃ¡ch hÃ ng?");
